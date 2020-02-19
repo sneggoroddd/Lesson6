cb0a501e3911b9a2db272dc21886febf85eef0cb
sneggorod2019 <sneggorod@mail.ru>
2020-02-19 21:58:10 +0300

    training in writing code

---
+++ functions.py
@@ -0,0 +1,8 @@
+def hello_who(name):
+    return f'Hello, {name}'
+def salary(hours, salary_by_hour):
+    '''
+    расчет зп сотрудника
+    '''
+    k=2
+    return hours*salary_by_hour*k
---
+++ simple_autotest.py
@@ -0,0 +1,3 @@
+from functions import salary, hello_who
+assert hello_who('Max') == 'Hello, Max' , 'hello_who error'
+assert salary(2,1) == 4 , 'salary error'

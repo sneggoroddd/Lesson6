c8fec0bf2cdc4ab7548ffd40306b4a0f04d7764d
sneggorod2019 <sneggorod@mail.ru>
2020-03-16 18:30:04 +0300

    training in writing code

--- test_functions.py
+++
@@ -1,9 +0,0 @@
-from functions import salary, hello_who
-
-def test_hello_who_max():
-    assert hello_who("Max") == "Hello Max"
-
-
-def test_hello_who_other():
-    assert hello_who("Tom") == "Hello, Tom"
-    assert hello_who("Bob") == "Hello, Bo"
